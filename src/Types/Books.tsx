5fb2ace0f1bbdf9ce70ce8a7e6ac538c33cdb378
nazmul <nazmulislam.ni897@gmail.com>
2023-06-09 07:08:29 +0600

    Add types

--- src/Types/Books.tsx
+++ src/Types/Books.tsx
@@ -1,7 +1,23 @@
 export type Books = {
   id: string;
+  title: string;
+  authors: string[];
+  publishedDate: string;
+  description: string;
+  pageCount: number;
+  categories: string[];
+  subCatagory: string[];
   imageLinks: {
     thumbnail: string;
+  };
+  language: string;
+  saleInfo: {
+    country: string;
+    price: number;
+    discountPrice: number;
+    availableCopy: number;
+    totalSales: number;
+    upto75off: boolean;
   };
 };
 
