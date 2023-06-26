6dd4c6087a4a923403f5a8f0ebc3378c4b54be87
nazmul <nazmulislam.ni897@gmail.com>
2023-06-26 11:53:01 +0600

    Add debounc and fixed fn

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -9,3 +9,21 @@ export function HeadingFormat(str: string) {
     .replace(/-/g, " ")
     .replace(/(?:^|\s)\S/g, (match) => match.toUpperCase());
 }
+
+export function fixed(num: number): number {
+  return +num.toFixed(2);
+}
+
+type CallbackFn<T extends any[]> = (...arg: T) => void;
+
+export function debounce<T extends any[]>(cb: CallbackFn<T>, delay: number) {
+  let timeout: NodeJS.Timeout;
+
+  return (...arg: T) => {
+    if (timeout) clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      cb(...arg);
+    }, delay);
+  };
+}
