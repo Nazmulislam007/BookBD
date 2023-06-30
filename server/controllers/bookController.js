1af7f3b2f15170e6f5b909a80aac33e78c71bca6
nazmul <nazmulislam.ni897@gmail.com>
2023-07-01 05:20:41 +0600

    Relatedbooks route

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -1,3 +1,4 @@
+const { Collection } = require("mongoose");
 const Book = require("../models/Books");
 
 const getAllBooks = async (req, res, next) => {
@@ -9,6 +10,62 @@ const getAllBooks = async (req, res, next) => {
   }
 };
 
+const getSingleBook = async (req, res, next) => {
+  try {
+    const _id = req.params._id;
+    const singleBook = await Book.findById({ _id });
+    res.status(200).json(singleBook);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const searchBooks = async (req, res, next) => {
+  try {
+    const { q } = req.query;
+
+    await Book.collection.createIndex({
+      title: "text",
+      subtitle: "text",
+      authors: "text",
+    });
+
+    const searchedBooks = await Book.find({
+      $text: {
+        $search: q,
+        $caseSensitive: false,
+      },
+    });
+
+    res.status(200).json(searchedBooks);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const relatedBooks = async (req, res, next) => {
+  try {
+    const { _ne, catagory, sub_catagory, _limit } = req.query;
+
+    const related = await Book.find({
+      _id: { $ne: _ne },
+      $or: [
+        { catagories: { $all: catagory } },
+        { subCatagory: { $all: sub_catagory } },
+      ],
+    })
+      .sort({ publishedDate: 1 })
+      .limit(_limit);
+
+    res.status(200).json(related);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getAllBooks,
+  getSingleBook,
+  searchBooks,
+  relatedBooks,
 };
--- server/router/bookRouter.js
+++ server/router/bookRouter.js
@@ -1,7 +1,17 @@
-const { getAllBooks } = require("../controllers/bookController");
+const {
+  getSingleBook,
+  getBooks,
+  getAllBooks,
+  searchBooks,
+  relatedBooks,
+} = require("../controllers/bookController");
 
 const router = require("express").Router();
 
 router.get("/", getAllBooks);
+router.get("/search", searchBooks);
+router.get("/related-books", relatedBooks);
+
+router.get("/:_id", getSingleBook);
 
 module.exports = router;
