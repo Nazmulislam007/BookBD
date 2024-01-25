eaa3764a71f2fc7e6a49c37dfeee847b946f1b54
nazmul <nazmulislam.ni897@gmail.com>
2024-01-25 14:02:07 +0600

    Add static images

---
+++ client/public/images/books/index.tsx
@@ -0,0 +1,71 @@
+import A_City from "./A City.jpg";
+import After_Death from "./After Death.jpg";
+import As_a from "./As a.jpg";
+import Cant_Hurt from "./Cant Hurt.jpg";
+import Creativity_Inc from "./Creativity Inc.jpg";
+import Crucial_Conversations from "./Crucial Conversations.jpg";
+import Disruptive_Thinking from "./Disruptive Thinking.jpg";
+import Dust_Book from "./Dust Book.jpg";
+import Elon_Musk from "./Elon Musk.jpg";
+import Game_Changer from "./Game Changer.jpg";
+import How_Not from "./How Not.jpg";
+import Is_Math from "./Is Math.jpg";
+import Lighter_Let from "./Lighter Let.jpg";
+import Mathematics_for from "./Mathematics for.jpg";
+import Oliver_Byrne from "./Oliver Byrne.jpg";
+import Project_Hail from "./Project Hail.jpg";
+import Read_Me from "./Read Me.jpg";
+import Ready_Player from "./Ready Player.jpg";
+import Shift_Silo from "./Shift Silo.jpg";
+import Soul_Boom from "./Soul Boom.jpg";
+import Stop_Overthinking from "./Stop Overthinking.jpg";
+import The_7 from "./The 7.jpg";
+import The_Architecture from "./The Architecture.jpg";
+import The_Highly from "./The Highly.jpg";
+import The_Little from "./The Little.jpg";
+import The_Man from "./The Man.jpg";
+import The_Mountain from "./The Mountain.jpg";
+import The_Power from "./The Power.jpg";
+import The_Ride from "./The Ride.jpg";
+import The_Right from "./The Right.jpg";
+import The_Ten from "./The Ten.jpg";
+import Think_and from "./Think and.jpg";
+import Titan_The from "./Titan The.jpg";
+import Unreasonable_Hospitality from "./Unreasonable Hospitality.jpg";
+
+export {
+  A_City,
+  After_Death,
+  As_a,
+  Cant_Hurt,
+  Creativity_Inc,
+  Crucial_Conversations,
+  Disruptive_Thinking,
+  Dust_Book,
+  Elon_Musk,
+  Game_Changer,
+  How_Not,
+  Is_Math,
+  Lighter_Let,
+  Mathematics_for,
+  Oliver_Byrne,
+  Project_Hail,
+  Read_Me,
+  Ready_Player,
+  Shift_Silo,
+  Soul_Boom,
+  Stop_Overthinking,
+  The_7,
+  The_Architecture,
+  The_Highly,
+  The_Little,
+  The_Man,
+  The_Mountain,
+  The_Power,
+  The_Ride,
+  The_Right,
+  The_Ten,
+  Think_and,
+  Titan_The,
+  Unreasonable_Hospitality,
+};
